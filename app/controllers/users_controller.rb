6fbd2b267c8de35ad5c9d8cad19bc769e8524b75
Savannah Scott <sascott624@gmail.com>
2016-09-14 14:38:54 -0400

    created users_controller.rb

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,47 @@
+class UsersController < ApplicationController
+
+  before_action find_user, only: [:show, :update, :edit, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+  end
+
+  def update
+    @user.update(user_params)
+    if @user.save
+      render json: @user
+    end
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  def edit
+  end
+
+  private
+
+  def find_user
+    @user = User.find_by(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+end
